646aadd3c9989c3cc0c02b8abf95a649708e9d79
Your Name <your-email@example.com>
2026-07-02 14:40:26 +0100

    commit

--- login.php
+++ login.php
@@ -135,7 +135,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login_type']) && $_PO
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | 5G E-GURUSCHOOL</title>
+    <title>Login | Madrasatu Masjid Ibn Abbas</title>
     <link rel="shortcut icon" href="assets/images/logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
